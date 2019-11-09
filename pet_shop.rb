14278f02cce34d7aa5cc50d2caad72cf7e5cc335
Alan <spambox+github@zenteck.co.uk>
2019-11-09 09:58:32 +0000

    breed

--- pet_shop.rb
+++ pet_shop.rb
@@ -10,8 +10,30 @@ def add_or_remove_cash(shop_name, amount)
   return shop_name[:admin][:total_cash] += amount
 end
 
-def pets_sold(customers)
-  for customer in customers
-    
+def pets_sold(shop_name)
+    return shop_name[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop_name, number_of_pets_sold)
+  return shop_name[:admin][:pets_sold] += number_of_pets_sold
+end
+
+def stock_count(shop_name)
+  return shop_name[:pets].count
+end
+
+def pets_by_breed(shop_name, breed)
+count_breed = []
+for pet in shop_name[:pets]
+  if pet[:breed] == breed
+count_breed.push(pet[:name])
+  end
+end
+return count_breed
+end
+
+def find_pet_by_name(shop_name, pet_name)
+for pet in shop_name[:pets]
+  
 
 end
