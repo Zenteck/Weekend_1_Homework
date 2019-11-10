7e562dedf9d753440b573928f09997e563e643db
Alan <spambox+github@zenteck.co.uk>
2019-11-10 18:54:31 +0000

    motorgrill

--- pet_shop.rb
+++ pet_shop.rb
@@ -33,23 +33,54 @@ return count_breed
 end
 
 def find_pet_by_name(shop_name, pet_name)
+pet_found = nil
   for pet in shop_name[:pets]
     if pet[:name] == pet_name
-      return pet_name, pet
-    else
-      return nil
+
+    end
+  end
+  return pet_name
 end
-end
-end
-#This works, but I don't think it's right.
+
+# This works, but I don't think it's right.
 
 # def add_pet_to_stock(shop_name, new_pets)
 #   for pet in new_pets
 #     shop_name[:pets] << pet
 # end
 # end
-#This should to be able to add multiple new stock pets if required
+#This 'should' to be able to add multiple new stock pets if required
+#But the above returned 10, I think it was adding every item in the pet hash separately
 
 def add_pet_to_stock(shop_name, new_pet)
     shop_name[:pets] << new_pet
+end
+
+# def customer_cash(customers)
+#   most_cash = 0
+#   for customer in customers
+#     if (customer[:cash] > most_cash)
+#       most_cash = customer[:cash]
+#       end
+#     end
+# end
+
+def customer_cash(customer)
+return customer[:cash]
+end
+#I JUST WASTED AN HOUR TRYING TO FIGURE OUT WHY 'CUSTOMER' WAS CALLED WITH AN IMPLICIT ARRAY ARGUMENT!!!
+#arguement is the array @customers with the first position marked
+#why does the @customers call always have array place 0 defined?
+
+# def remove_customer_cash(customer, cost)
+#
+#
+# end
+
+def remove_customer_cash(customer, cost)
+  return customer[:cash] -= cost
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
 end
